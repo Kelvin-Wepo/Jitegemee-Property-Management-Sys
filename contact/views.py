be58168a9bc89f9616f48f70022e947fef334322
unknown <mwangielizabeth050@gmail.com>
2024-04-24 10:59:05 +0300

    sms addition

--- contact/views.py
+++ contact/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.models import User
 from django.contrib import messages
-
+import africastalking
 from .models import Contact
+
+from django.conf import settings
+africastalking.api_key = settings.AFRICASTALKING_API_KEY
 
 class ContactView(View):
 
@@ -34,6 +37,15 @@ class ContactView(View):
             contact.save()
 
             messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
+            africastalking_username = 'lyzy'
+            africastalking_api_key = africastalking.api_key
+                
+            africastalking.initialize(africastalking_username, africastalking_api_key)
+            sms = africastalking.SMS
+            message = "Your request has been submitted, a realtor will get back to you soon"
+            response = sms.send(message, [phone])
+            #print(profile.contact_number)
+            print("SMS response:", response)
             return redirect('/listing/'+listing_id)
 
 
